0b859774cedbc2576698264bbf6eeddbf445ff1b
Maind1957 <116671611+Maind1957@users.noreply.github.com>
2022-12-07 15:57:42 +0800

    fix bug

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -89,7 +89,7 @@ export class MainComponent implements OnInit {
     return [header, body.join("")]
   }
 
-  private static decodeBase64(base64) {
+  private decodeBase64(base64) {
     const converted_function = atob(base64);
     const length = converted_function.length;
     const bytes = new Uint8Array(length);
